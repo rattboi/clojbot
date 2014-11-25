6ad5b3f67209b39b96f8606c37cdb4fb4ef8b117
Bradon Kanyid <rattboi@gmail.com>
2014-11-24 17:36:23 -0800

    Added initial simple routes

--- src/clojbot/core.clj
+++ src/clojbot/core.clj
@@ -1,8 +1,33 @@
 (ns clojbot.core
   (:require [irclj.core :as irc]))
 
+(defn handle-privmsg [conn args]
+  (let [{:keys [text 
+                target 
+                nick 
+                host 
+                command]} args]
+    (prn command text target nick host)  
+    (irc/message conn target (str "Hi " nick "! You said: " text))))
+
+(defn handle-notice [conn args]
+  (let [{:keys [text 
+                target 
+                nick 
+                host 
+                command]} args]
+    (prn command nick)))
+
+(defn handle-ctcp [conn args]
+  (prn args))
+
 (defn -main [& args]
-  (let [conn (irc/connect "irc.cat.pdx.edu" 6697 "clojbot" :ssl? true)
+  (let [server "irc.cat.pdx.edu"
+        port 6697
+        bot-nick "clojbot"
+        ssl  true 
+        callbacks {:privmsg handle-privmsg :notice handle-notice :ctcp-action handle-ctcp}
+        conn (irc/connect server port bot-nick :ssl? ssl :callbacks callbacks)
         chan "#botgrounds"]
     (irc/join conn chan)
     (irc/message conn chan "Hello, World!")
